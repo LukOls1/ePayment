62356b581646d470062e3fb9f5735374f871871d
LukOls1 <lukasz.olszyk@gmail.com>
2018-02-07 23:08:47 +0100

    dashboard_view

--- js/views/dashboard_view.js
+++ js/views/dashboard_view.js
@@ -15,10 +15,75 @@ DashboardView.prototype.render = function() {
 DashboardView.prototype.createNavigationElement = function() {
   var navigation = document.createElement('div');
   $(navigation).attr('id', 'navigation');
-  navigation.innerText = "Navigation";
+  $(navigation).append(this.createNavbarHeader);
+
   return navigation;
 }
-
+DashboardView.prototype.createTopBar = function() {
+  var navigation = document.createElement('div');
+  $(navigation).addClass('navbar navbar-inverse');
+  $(navigation).append(this.createContainerr);
+  
+  return navigation;
+}
+DashboardView.prototype.createContainerr = function() {
+  var navigation = document.createElement('div');
+  $(navigation).addClass('navbar navbar-inverse');
+  $(navigation).append(this.createContainer);
+  
+  return navigation;
+}
+console.log('jajebe')
+DashboardView.prototype.createContainer = function() {
+  console.log('jajebe')
+  var navigation = document.createElement('div');
+  $(navigation).addClass('container-fluid');
+  $(navigation).append(this.createNavbarHeader);
+  
+  return navigation;
+}
+DashboardView.prototype.createNavbarHeader = function() {
+  var navigation = document.createElement('div');
+  $(navigation).addClass('navbar-header');
+  $(navigation).append(this.createLogo);
+  
+  return navigation;
+}
+DashboardView.prototype.createLogo = function() {
+  var navigation = document.createElement('a');
+  $(navigation).addClass('navbar-brand');
+  $(navigation).text('ePayment');
+  
+  return navigation;
+}
+DashboardView.prototype.createNavbarTopNavbarList = function() {
+  var navigation = document.createElement('ul');
+  $(navigation).addClass('nav navbar-nav');
+  $(navigation).append(this.createNavbarHome);
+  $(navigation).append(this.createNavbarMessages);
+  $(navigation).append(this.createNavbarPeople);
+  
+  return navigation;
+}
+DashboardView.prototype.createNavbarHome = function() {
+  var navigation = document.createElement('li');
+  $(navigation).addClass('active');
+  $(navigation).text('Home');
+  
+  return navigation;
+}
+DashboardView.prototype.createNavbarMessages = function() {
+  var navigation = document.createElement('li');
+  $(navigation).text('Messages');
+  
+  return navigation;
+}
+DashboardView.prototype.createNavbarPeople = function() {
+  var navigation = document.createElement('li');
+  $(navigation).text('People');
+  
+  return navigation;
+}
 DashboardView.prototype.createMainContentElement = function() {
   var mainContent = document.createElement('div');
   $(mainContent).attr('id', 'mainContent');
