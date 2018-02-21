b4679d194c5c453f0fcc70b880dcc3c1e4442ee2
szymongor <gornioczek.szymon@gmail.com>
2018-02-21 15:37:31 +0100

    Payment list

--- js/views/payments_view/payments_list.js
+++ js/views/payments_view/payments_list.js
@@ -1,5 +1,44 @@
 function PaymentsList(parentId) {
   this.parentId = parentId;
+
+  this.listHeaderElements = [
+    { name: 'Type', style: 'col-sm-1 col-xs-2 dev-border' },
+    { name: 'Title', style: 'col-sm-2 col-xs-2 dev-border' },
+    { name: 'Progress', style: 'col-sm-6 col-xs-5 dev-border' },
+    { name: 'Status', style: 'col-sm-2 col-xs-2 dev-border' }
+  ];
+
+  this.payments = [
+    {
+      type: 'CZYNSZ',
+      title: 'Czynsz 2',
+      toPay: 280,
+      paid: 123,
+      status: 'pending'
+    },
+    { type: 'PRAD', title: 'Prąd 2', toPay: 120, paid: 100, status: 'pending' },
+    {
+      type: 'CZYNSZ',
+      title: 'Czynsz 1',
+      toPay: 280,
+      paid: 123,
+      status: 'pending'
+    },
+    {
+      type: 'NET',
+      title: 'Internet 2.02',
+      toPay: 40,
+      paid: 5,
+      status: 'pending'
+    },
+    {
+      type: 'CZYNSZ',
+      title: 'Czynsz 0',
+      toPay: 280,
+      paid: 280,
+      status: 'pending'
+    }
+  ];
 }
 
 PaymentsList.prototype.render = function() {
@@ -10,13 +49,13 @@ PaymentsList.prototype.render = function() {
 PaymentsList.prototype.createPaymentsList = function() {
   console.log('PaymentsList create');
   var list = document.createElement('ul');
-  $(list).addClass(' panel panel-default list-group dev-border');
+  $(list).addClass('panel panel-default list-group dev-border ');
 
   $(list).append(this.createPaymentsListTitle());
 
   $(list).append(this.createPaymentsListHeader());
-  for (i = 0; i < 4; i++) {
-    $(list).append(this.createPaymentsListElement('Element 1'));
+  for (i = 0; i < this.payments.length; i++) {
+    $(list).append(this.createPaymentsListElement(this.payments[i]));
   }
   return list;
 };
@@ -39,9 +78,9 @@ PaymentsList.prototype.createPaymentsListHeader = function() {
 
   var listHeader = document.createElement('h4');
   $(listHeader).addClass('list-group-item-heading dev-border');
-  $(listHeader).append(this.createHeaderElement('Title'));
-  $(listHeader).append(this.createHeaderElement('Progress'));
-  $(listHeader).append(this.createHeaderElement('Status'));
+  for (i = 0; i < this.listHeaderElements.length; i++) {
+    $(listHeader).append(this.createHeaderElement(this.listHeaderElements[i]));
+  }
 
   $(row).append(listHeader);
   return container;
@@ -49,8 +88,8 @@ PaymentsList.prototype.createPaymentsListHeader = function() {
 
 PaymentsList.prototype.createHeaderElement = function(elementData) {
   var headerElement = document.createElement('div');
-  $(headerElement).addClass('col-sm-4 col-xs-4 dev-border');
-  $(headerElement).append(elementData);
+  $(headerElement).addClass(elementData.style);
+  $(headerElement).append(elementData.name);
   return headerElement;
 };
 
@@ -60,9 +99,14 @@ PaymentsList.prototype.createPaymentsListElement = function(elementData) {
   var element = document.createElement('a');
   $(element).addClass('list-group-item');
 
-  var title = this.createPaymentTitle('lololol');
-  var proggressBar = this.createPaymentProgress(120, 280);
+  var type = this.createPaymentType(elementData.type);
+  var title = this.createPaymentTitle(elementData.title);
+  var proggressBar = this.createPaymentProgress(
+    elementData.paid,
+    elementData.toPay
+  );
 
+  $(elementWrapper).append(type);
   $(elementWrapper).append(title);
   $(elementWrapper).append(proggressBar);
 
@@ -70,16 +114,23 @@ PaymentsList.prototype.createPaymentsListElement = function(elementData) {
   return element;
 };
 
+PaymentsList.prototype.createPaymentType = function(type) {
+  var typeDiv = document.createElement('div');
+  $(typeDiv).addClass('col-sm-1 col-xs-2 dev-border');
+  $(typeDiv).append('<span class="glyphicon glyphicon-home"></span>');
+  return typeDiv;
+};
+
 PaymentsList.prototype.createPaymentTitle = function(title) {
   var titleDiv = document.createElement('div');
-  $(titleDiv).addClass('col-sm-4 col-xs-4 dev-border');
+  $(titleDiv).addClass('col-sm-2 col-xs-2 dev-border');
   $(titleDiv).append(title);
   return titleDiv;
 };
 
 PaymentsList.prototype.createPaymentProgress = function(paid, toPay) {
   var progressWrapper = document.createElement('div');
-  $(progressWrapper).addClass('col-sm-6 col-xs-6 dev-border');
+  $(progressWrapper).addClass('col-sm-6 col-xs-5 dev-border');
 
   var progress = document.createElement('div');
   $(progress).addClass('progress');
