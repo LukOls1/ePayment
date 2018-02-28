9f32274f5e2b96f861b3860184b8ded3157314b7
LukOls1 <lukasz.olszyk@gmail.com>
2018-02-28 17:36:31 +0100

    posts_view+posts

--- js/api_client.js
+++ js/api_client.js
@@ -1,6 +1,37 @@
 function ApiClient() {
-
+  this.serverUrl = 'http://epaymentbema.pythonanywhere.com/';
 }
+
+ApiClient.prototype.setRequestHeaders = function(request) {
+  request.setRequestHeader(
+    'Authorization',
+    'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6IkhpcGV4IiwiZXhwIjoxNTE5NzU2ODA0LCJlbWFpbCI6IiJ9.OZu2GVeM0msTanvD3phlfwdCw_kZLn0ZzY8xwVmbzWI'
+  );
+  request.setRequestHeader('Content-Type', 'application/json');
+}
+
+ApiClient.prototype.getPosts = function(callback) {
+  $.ajax({
+    type: 'GET',
+    url: this.serverUrl + 'posts/',
+    beforeSend: this.setRequestHeaders,
+    success: function(data) {
+      callback(data);
+    }
+  });
+};
+
+ApiClient.prototype.createPost = function(post, callback) {
+  $.ajax({
+    type: 'POST',
+    url: this.serverUrl + 'posts/',
+    beforeSend: this.setRequestHeaders,
+    data: JSON.stringify({ post: post}),
+    success: function(data) {
+      callback(data);
+    }
+  });
+};
 
 ApiClient.prototype.login = function(login, password, callback) {
   if (login === "Kubix") {
