d030f86672fe0f2394ac96bef665662f5feb9b97
szymongor <gornioczek.szymon@gmail.com>
2018-03-05 14:14:17 +0100

    Login provider

--- js/app.js
+++ js/app.js
@@ -1,26 +1,24 @@
 var app;
 $(document).ready(function() {
   app = new App();
-  app.render()
+  app.render();
 });
 
 function App() {
-  this.state = new State();             /*przypisuje do pola state obiekt state  */
-  this.apiClient = new ApiClient();     /*przypisuje do pola state obiekt apiclient  */
-  this.currentView;                     /*przetrzymuje obiekt obecnego widoku  */
+  this.state = new State(); /*przypisuje do pola state obiekt state  */
+  this.apiClient = new ApiClient(); /*przypisuje do pola state obiekt apiclient  */
+  this.currentView; /*przetrzymuje obiekt obecnego widoku  */
   this.initView();
 }
 
 App.prototype.initView = function() {
-  if (this.state.isLoggedOn()){
+  if (this.state.isLoggedOn()) {
     this.currentView = new DashboardView(); /*wywołanie konstruktora*/
+  } else {
+    this.currentView = new LoginView(); /*wyswietla aktualnie*/
   }
-  else {
-    this.currentView = new DashboardView(); /*wyswietla aktualnie*/
-  }
-}
+};
 
 App.prototype.render = function() {
   this.currentView.render();
-
-}
+};
